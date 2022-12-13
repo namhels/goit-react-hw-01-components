f600a1e1c97e7f4fa377723781baa9e45f71b015
namhels <8.lsv.888@gmail.com>
2022-12-13 21:12:01 +0300

    start hw

---
+++ src/components/App.jsx
@@ -0,0 +1,16 @@
+export const App = () => {
+  return (
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101'
+      }}
+    >
+      Hi 🍀🚀😎 React
+    </div>
+  );
+};
